c7f2bef9cf00ab509a84213ac94a10342f147fdd
Prerak126 <prerak126@gmail.com>
2017-08-11 11:11:19 -0400

    Array 1

---
+++ 1.js
@@ -0,0 +1,10 @@
+var a=["One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"];
+
+function displayQuotes() {
+    for(var i=0;i<a.length;i++){
+        console.log(a[i]);
+        console.log("<br>");
+    }
+}
+
+displayQuotes();
