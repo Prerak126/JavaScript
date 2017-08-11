03e637803c258a9cecbecd6756f839440021c6a7
Prerak126 <prerak126@gmail.com>
2017-08-11 11:13:09 -0400

    Array 3

---
+++ 3.js
@@ -0,0 +1,19 @@
+var a=["One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"];
+
+a.pop();
+
+document.write("Pop called, check console for length ");
+console.log(a.length);
+
+document.write("<br>");
+
+document.write(a.reverse());
+
+console.log(a[7]);
+document.write("<br>");
+
+document.write(a.shift());
+
+console.log(a.length);
+document.write("<br>");
+document.write(a.sort());
